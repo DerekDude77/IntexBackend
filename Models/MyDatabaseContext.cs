1a29f174066c6e9c809e89473e668b3ded5661c9
Derek Charlton <dc455@byu.edu>
2023-04-12 17:04:17 -0600

    Add project files.

---
+++ Models/MyDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IntexDatabaseConnection.Models
+{
+    public class MyDatabaseContext : DbContext
+    {
+
+        public MyDatabaseContext(DbContextOptions<MyDatabaseContext> options)
+        : base(options)
+        {
+        }
+
+        //public DbSet<YourModel> YourModels { get; set; }
+        // Add more DbSet properties for your other tables
+
+    }
+}
+
+// CONFIGURE THIS TO MATCH THE DATA CONTAINED IN THE POSTGRESQL DATABASE
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore; // Replace 'YourNamespace' with the namespace of your MyDatabaseContext class
+using IntexDatabaseConnection.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MyDatabaseContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("MyDatabase")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
